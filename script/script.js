b88981d2727819c9a47d2aa21ef3a70f08e745e7
Marat141 <terest141@gmail.com>
2024-11-18 09:32:07 +0100

    1

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const nabidka = ["CPU", "GPU", "RAM"];
+const kosik = []
+
+function vypis(array, output){
+    document.getElementById(output).innerHTML = "";
+    for(let i =0; i<array.length; i++){
+        document.getElementById(output).innerHTML += i+1 + ". " + array[i] + "<br>";
+    }
+}
+console.log(nabidka);
+
+function Add(){
+    const input = document.getElementById("add-component")
+    const content = document.getElementById("content")
+    const user_cart = document.getElementById("user-cart")
+
+    if((input.value<nabidka.length) && (input.value> 0) && (!isNaN (input.value))) {
+        kosik.push(nabidka[input.value -1]);
+    }
+    else{
+        alert("Chyba");
+    }
+    vypis(kosik, "user-cart");
+}
+
+vypis(nabidka, "content");
